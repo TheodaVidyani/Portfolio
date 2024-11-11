f0f95ad1447d61bd006001242be5982346143f6f
TheodaVidyani <theodahettiarachchi00@gmail.com>
2024-11-11 08:42:05 +0530

    1111

--- src/app/beyond_work/page.jsx
+++ src/app/beyond_work/page.jsx
@@ -7,36 +7,48 @@ const BeyondWork = () => {
   const works = [
     {
       id: 1,
+      imageSrc: "/RotaTreasurer.jpg",
+      description:
+        "I have taken on the role of Treasurer of the Rotaract Club of the University of Moratuwa for the RI year 2024-25 during the 29th installation ceremony that took place on November 3rd! It’s an incredible honor to serve alongside such passionate and dedicated individuals, and I’m looking forward to making this year one of impact, growth, and meaningful connections.",
+    },
+    {
+      id: 2,
+      imageSrc: "/IEEE.jpg",
+      description:
+        "Proud to have participated in HackElite 1.0 with Team ThreeBestBits! It was an inspiring journey of creativity and collaboration. Huge thanks to the IEEE WIE Student Branch, University of Moratuwa, for hosting this memorable event!",
+    },
+    {
+      id: 3,
       imageSrc: "/Rota ClubService.jpg",
       description:
         "I have taken on the role of Co-Director of the Club Service Avenue of the Rotaract Club of the University of Moratuwa for the RI year 2023-24 during the 28th installation ceremony that took place on September 24th! It was a great platform to contribute my skills and collaborate with a fantastic RotaractMora team to make our projects even more successful.",
     },
     {
-      id: 2,
+      id: 4,
       imageSrc: "/UniCode.jpg",
       description:
         "Team UniCode secured the 9th place in Coderush 2023, organized by INTECS on the 4th of November 2023 at University of Moratuwa premises.This accomplishment was made possible through the incredible teamwork, dedication, and support of my talented team members. Grateful for the shared effort that led us to this milestone!",
     },
     {
-      id: 3,
+      id: 5,
       imageSrc: "/HackMoral.jpg",
       description:
         "I was thrilled to participate in HackMoral 6.0, a mini hackathon organized by INTECS at the Faculty of Information Technology, University of Moratuwa. Grateful for the opportunity to put my skills to the test.",
     },
     {
-      id: 4,
+      id: 6,
       imageSrc: "/DataStorm.jpg",
       description:
         "I got the opportunity to chair Data Storm 4.0, an Advanced Analytics Competition organized by the Rotaract Clubs of the University of Moratuwa and the Faculty of Science, University of Colombo, and powered by OCTAVE - John Keells Group (April 2022). The experience was truly memorable and filled with invaluable lessons.",
     },
     {
-      id: 5,
+      id: 7,
       imageSrc: "/EGallella.jpg",
       description:
         "Honored to have secured 1st place in the Verse Category at E-Gallella 2022, organized by the IT Faculty of the University of Moratuwa. This experience was truly rewarding, and I am grateful for the opportunity to showcase my skills and be part of such a prestigious event!.",
     },
     {
-      id: 6,
+      id: 8,
       imageSrc: "/UCSC.jpg",
       description:
         "I'm very excited to share that I've reached a significant milestone in my Bachelor of Information Technology (BIT) degree from the University of Colombo by successfully completing the first year. This accomplishment has been rewarded with a Diploma in Information Technology (DIT), marking a proud step forward in my academic journey!",
